0a0230835338a3a4f14cdfda271a82da231b2850
Olivier Scherrer <pode.fr@gmail.com>
2014-04-10 23:31:29 -0400

    add implementation

--- index.js
+++ index.js
@@ -6,3 +6,26 @@
 * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
 */
 "use strict";
+
+/**
+ * Get a domNode's dataset attribute. If dataset doesn't exist (IE)
+ * then the domNode is looped through to collect them.
+ * @param {HTMLElement|SVGElement} dom
+ * @returns {Object} dataset
+ */
+ module.exports = function getDataset(dom) {
+    var dataset = {},
+        i, i, split,join;
+
+    if ("dataset" in dom) {
+        return dom.dataset;
+    } else {
+        for (i=0, l=dom.attributes.length; i<l; i++) {
+            split = dom.attributes[i].name.split("-");
+            if (split.shift() == "data") {
+                dataset[join = split.join("-")] = dom.getAttribute("data-"+join);
+            }
+        }
+        return dataset;
+    }
+};
