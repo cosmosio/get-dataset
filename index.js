7f3d470e1a17cdc272226c551ce56bced2dbce85
Olivier Scherrer <pode.fr@gmail.com>
2014-04-10 23:36:15 -0400

    add tests

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
  */
  module.exports = function getDataset(dom) {
     var dataset = {},
-        i, i, split,join;
+        i, l, split,join;
 
     if ("dataset" in dom) {
         return dom.dataset;
--- test/get-dataset.spec.js
+++ test/get-dataset.spec.js
@@ -5,3 +5,38 @@
 *
 * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
 */
+
+require("quick-dom");
+
+var getDataset = require("../index");
+
+describe("get-dataset", function () {
+
+    var dom;
+
+    beforeEach(function () {
+        dom = document.createElement("div");
+        dom.setAttribute("data-plugin1", "name1");
+        dom.setAttribute("data-plugin2", "name2");
+    });
+
+    it("should be a function", function () {
+        expect(typeof getDataset).toBe("function");
+    });
+
+    it("should return an object similar to a dataset", function () {
+        var dataset = getDataset(dom);
+        expect(typeof dataset).toBe("object");
+        expect(dataset.plugin1).toBe("name1");
+        expect(dataset.plugin2).toBe("name2");
+    });
+
+    it("should exactly return the dataset if the browser supports it", function () {
+        if (dom.dataset) {
+            dom.dataset.plugin1 = "browser supports dataset";
+            var dataset = getDataset(dom);
+            expect(dataset.plugin1).toBe("browser supports dataset");
+        }
+    });
+
+});
