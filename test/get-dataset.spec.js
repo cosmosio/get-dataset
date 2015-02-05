1c3fb7b9ca7af1e1a37db2329ad3e90d4c853624
Olivier Scherrer <pode.fr@gmail.com>
2015-02-04 22:20:00 -0500

    Bump to 0.0.2 and update tests to illustrate #1 and use mocha

--- test/get-dataset.spec.js
+++ test/get-dataset.spec.js
@@ -3,11 +3,13 @@
 *
 * The MIT License (MIT)
 *
-* Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+* Copyright (c) 2014-2015 Olivier Scherrer <pode.fr@gmail.com>
 */
 
 require("quick-dom");
 
+var chai = require("chai");
+var expect = chai.expect;
 var getDataset = require("../index");
 
 describe("get-dataset", function () {
@@ -16,26 +18,20 @@ describe("get-dataset", function () {
 
     beforeEach(function () {
         dom = document.createElement("div");
-        dom.setAttribute("data-plugin1", "name1");
-        dom.setAttribute("data-plugin2", "name2");
-    });
-
-    it("should be a function", function () {
-        expect(typeof getDataset).toBe("function");
+        dom.setAttribute("data-plugin", "name1");
+        dom.setAttribute("data-plugin-composed-name", "name2");
     });
 
     it("should return an object similar to a dataset", function () {
-        var dataset = getDataset(dom);
-        expect(typeof dataset).toBe("object");
-        expect(dataset.plugin1).toBe("name1");
-        expect(dataset.plugin2).toBe("name2");
+        expect(getDataset(dom)).to.eql({
+            plugin: "name1",
+            pluginComposedName: "name2"
+        });
     });
 
     it("should exactly return the dataset if the browser supports it", function () {
         if (dom.dataset) {
-            dom.dataset.plugin1 = "browser supports dataset";
-            var dataset = getDataset(dom);
-            expect(dataset.plugin1).toBe("browser supports dataset");
+            expect(dom.dataset).to.be(getDataset(dom));
         }
     });
 
